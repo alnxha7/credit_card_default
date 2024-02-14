b6f30f822fca9cd6fd8f2251120e3b095e49ff44
[alansha m] <[alns4rha@gmail.com]>
2024-02-14 23:33:56 +0530

    built with cassandra db now

--- app.py
+++ app.py
@@ -1,20 +1,61 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 import pickle
+import json
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-app.config['MONGO_URI'] = 'mongodb+srv://alns4rha:alansha@cluster0.najsjgs.mongodb.net/Collection_1?retryWrites=true&w=majority'
+cloud_config= {
+  'secure_connect_bundle': 'secure-connect-credit-card.zip'
+}
+with open("credit_card-token.json") as f:
+    secrets = json.load(f)
 
-mongo = PyMongo(app)
+CLIENT_ID = secrets["clientId"]
+CLIENT_SECRET = secrets["secret"]
 
-if mongo.db:
-    Collection_1 = mongo.db.Collection_1
-else:
-    print("Error: MongoDB connection failed")
+auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+keyspace_name = 'collection_1'
+session.set_keyspace(keyspace_name)
+
+table_name = 'credit_card_new'
+
+create_table_query = f"""
+CREATE TABLE IF NOT EXISTS {keyspace_name}.{table_name} (
+    gender INT,
+    education INT,
+    marrital_status INT,
+    age INT,
+    limit_balance INT,
+    pay_1 INT,
+    pay_2 INT,
+    pay_3 INT,
+    pay_4 INT,
+    pay_5 INT,
+    pay_6 INT,
+    bill_amt1 INT,
+    bill_amt2 INT,
+    bill_amt3 INT,
+    bill_amt4 INT,
+    bill_amt5 INT,
+    bill_amt6 INT,
+    pay_amt1 INT,
+    pay_amt2 INT,
+    pay_amt3 INT,
+    pay_amt4 INT,
+    pay_amt5 INT,
+    pay_amt6 INT,
+    prediction INT,
+    PRIMARY KEY (gender, education, marrital_status, age, limit_balance)
+)
+"""
+session.execute(create_table_query)
 
 @app.route('/')
 def home():
@@ -22,60 +63,83 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-
-    # Collection_1 = mongo.db.Collection_1
-
     '''
     for rendering results on HTML
     '''
     features = [int(x) for x in request.form.values()]
 
-    # re-arranging the list as per data set
+    # re-arranging the list as per the data set
     feature_list = [features[4]] + features[:4] + features[5:11][::-1] + features[11:17][::-1] + features[17:][::-1]
     features_arr = [np.array(feature_list)]
 
     prediction = model.predict(features_arr)
 
     print(features_arr)
-    #newdb.insert_one(d)
-    print("features is :",features)
-    default_payment=prediction.tolist()
+    default_payment = prediction.tolist()
 
-    if mongo.db:
-        # Save the prediction result to the MongoDB collection
+    query = f"""
+    INSERT INTO {table_name} (
+    gender, education, marrital_status, age, limit_balance,
+    pay_1, pay_2, pay_3, pay_4, pay_5, pay_6,
+    bill_amt1, bill_amt2, bill_amt3, bill_amt4, bill_amt5, bill_amt6,
+    pay_amt1, pay_amt2, pay_amt3, pay_amt4, pay_amt5, pay_amt6, prediction
+)
+VALUES (
+    :gender, :education, :marrital_status, :age, :limit_balance,
+    :pay_1, :pay_2, :pay_3, :pay_4, :pay_5, :pay_6,
+    :bill_amt1, :bill_amt2, :bill_amt3, :bill_amt4, :bill_amt5, :bill_amt6,
+    :pay_amt1, :pay_amt2, :pay_amt3, :pay_amt4, :pay_amt5, :pay_amt6, :prediction
+)
+"""
 
-        New_database = {
-            'Gender': features[0],
-            'Education': features[1],
-            'Marrital Status': features[2],
-            'Age': features[3],
-            'Limit Balance': features[4],
-            'PAY_1': features[5],
-            'PAY_2': features[6],
-            'PAY_3': features[7],
-            'PAY_4': features[8],
-            'PAY_5': features[9],
-            'PAY_6': features[10],
-            'BILL_AMT1': features[11],
-            'BILL_AMT2': features[12],
-            'BILL_AMT3': features[13],
-            'BILL_AMT4': features[14],
-            'BILL_AMT5': features[15],
-            'BILL_AMT6': features[16],
-            'PAY_AMT1': features[17],
-            'PAY_AMT2': features[18],
-            'PAY_AMT3': features[19],
-            'PAY_AMT4': features[20],
-            'PAY_AMT5': features[21],
-            'PAY_AMT6': features[22],
-            'Prediction': default_payment[0]
-        }
-        Collection_1.insert_one(New_database)
-        print("Document inserted successfully")
-    else:
-        print("Error: MongoDB connection not available")
-   
-    print("prediction value: ", prediction)
+# Ensure that the parameters dictionary includes all required keys
+    parameters = {
+    'gender': features[0],
+    'education': features[1],
+    'marrital_status': features[2],
+    'age': features[3],
+    'limit_balance': features[4],
+    'pay_1': features[5],
+    'pay_2': features[6],
+    'pay_3': features[7],
+    'pay_4': features[8],
+    'pay_5': features[9],
+    'pay_6': features[10],
+    'bill_amt1': features[11],
+    'bill_amt2': features[12],
+    'bill_amt3': features[13],
+    'bill_amt4': features[14],
+    'bill_amt5': features[15],
+    'bill_amt6': features[16],
+    'pay_amt1': features[17],
+    'pay_amt2': features[18],
+    'pay_amt3': features[19],
+    'pay_amt4': features[20],
+    'pay_amt5': features[21],
+    'pay_amt6': features[22],
+    'prediction': default_payment[0]
+}
+
+    # Bind the parameters and execute the query
+    try:
+        session.execute(
+        f"INSERT INTO {keyspace_name}.{table_name} "
+        "(gender, education, marrital_status, age, limit_balance, "
+        "pay_1, pay_2, pay_3, pay_4, pay_5, pay_6, "
+        "bill_amt1, bill_amt2, bill_amt3, bill_amt4, bill_amt5, bill_amt6, "
+        "pay_amt1, pay_amt2, pay_amt3, pay_amt4, pay_amt5, pay_amt6, prediction) "
+        "VALUES (%(gender)s, %(education)s, %(marrital_status)s, %(age)s, %(limit_balance)s, "
+        "%(pay_1)s, %(pay_2)s, %(pay_3)s, %(pay_4)s, %(pay_5)s, %(pay_6)s, "
+        "%(bill_amt1)s, %(bill_amt2)s, %(bill_amt3)s, %(bill_amt4)s, %(bill_amt5)s, %(bill_amt6)s, "
+        "%(pay_amt1)s, %(pay_amt2)s, %(pay_amt3)s, %(pay_amt4)s, %(pay_amt5)s, %(pay_amt6)s, %(prediction)s)",
+        parameters
+    )
+        print("Document inserted successfully.")
+        # Add success message to return to the client
+    except Exception as e:
+        print(f"Error inserting document: {e}")
+
+    print("prediction value:", prediction)
 
     result = ""
     if prediction == 1:
@@ -83,8 +147,7 @@ def predict():
     else:
         result = "The Credit card holder will not be Defaulter in the next month"
 
-    return render_template('index.html', prediction_text = result)
-
+    return render_template('index.html', prediction_text=result)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
